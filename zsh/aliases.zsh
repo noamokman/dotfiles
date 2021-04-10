9d3057bd5d05cbfdaa5dcec5dead9130ee9593d8
Noam Okman <noamokman@gmail.com>
2021-04-10 08:28:01 +0300

    .

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,2 @@
-alias gcln='git fetch -p && git checkout -B master origin/master && git branch | grep -v "master" | xargs git branch -D'
-alias gcmain='git fetch -p && git checkout -B main origin/main && git branch | grep -v "main" | xargs git branch -D'
 alias frk='fork .'
-
-alias sso='java -jar ~/.sso/ssogenerator.jar'
 alias idcp='uuid | tr -d "\n" | pbcopy'
-alias killz='sudo launchctl remove com.zscaler.service && sudo launchctl remove com.zscaler.tunnel'
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,20 +1,34 @@
+git_default_branch() {
+    echo $(basename $(git symbolic-ref --short refs/remotes/origin/HEAD))
+}
+
 webclone() {
     hub clone soluto-private/${1}
     webstorm ./${1}
 }
 
 gsync() {
-    if ("$(git rev-parse --abbrev-ref HEAD)" == "master"); then
-        echo You are on master!
+    local git_default_branch=$git_main_branch
+    if ("$(git rev-parse --abbrev-ref HEAD)" == $git_default_branch); then
+        echo You are on $git_default_branch!
         return 1
     fi
+    message="$*"
     git add --all
-    git commit -am "${1=.}"
+    git commit -am "${message=.}"
     git push origin $(git rev-parse --abbrev-ref HEAD)
 }
 
-gmaster() {
+gsyncf() {
+    message="$*"
     git add --all
-    git commit -am "${1=fix}"
+    git commit -am "${message=.}"
     git push origin $(git rev-parse --abbrev-ref HEAD)
+}
+
+gcln() {
+    local git_default_branch=$(git_main_branch)
+    git fetch -p
+    git checkout -B $git_default_branch origin/$git_default_branch
+    git branch | grep -v "$git_default_branch" | xargs git branch -D
 }
